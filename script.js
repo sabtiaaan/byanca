2a9addd3ffa59a1d9f5315414368a747b58d8814
sabtiaaan <sabtiaaan@gmail.com>
2025-07-24 14:50:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function unlockGallery() {
   const password = document.getElementById("password-input").value.toLowerCase();
-  if (password === 'ikan goreng') {
+  if (password === 'lari pagi') {
     document.getElementById('lock-screen').style.display = 'none';
     document.getElementById('gallery').style.display = 'block';
   } else {
